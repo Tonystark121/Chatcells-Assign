5a5c74019910ad59a96bcf9ed2b0f75004727c0e
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2024-05-04 23:00:01 +0530

    structure added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        popins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#ff0000",
+        secondary: "#00ff00",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
